626a7a8871a5f6f77c0d67d175b9756c3bc81d01
Rohit1173 <lcs2020048@iiitl.ac.in>
2022-04-24 19:32:58 +0530

    fixes

--- app/src/main/java/com/example/cams/VolunteerToHelpFragment.kt
+++ app/src/main/java/com/example/cams/VolunteerToHelpFragment.kt
@@ -1,10 +1,13 @@
 package com.example.cams
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class VolunteerToHelpFragment : Fragment() {
@@ -15,7 +18,23 @@ class VolunteerToHelpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_volunteer_too_help, container, false)
+        val v= inflater.inflate(R.layout.fragment_volunteer_too_help, container, false)
+        val camgit:Button=v.findViewById(R.id.camsgit)
+        val project:Button=v.findViewById(R.id.project)
+        val ask:Button=v.findViewById(R.id.ask)
+        camgit.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW).apply { data = Uri.parse("https://github.com/CriticalAssetManagement") }
+            startActivity(i)
+        }
+        project.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW).apply { data = Uri.parse("https://github.com/orgs/CriticalAssetManagement/projects/1") }
+            startActivity(i)
+        }
+        ask.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW).apply { data = Uri.parse("https://github.com/CriticalAssetManagement/CAMS/discussions") }
+            startActivity(i)
+        }
+        return v
     }
 
 
