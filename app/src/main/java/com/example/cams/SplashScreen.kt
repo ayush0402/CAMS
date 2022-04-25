58ac23a2facc087554cac12dd3aa84953ae09c5e
Rishi Raj <rishiraj727909@gmail.com>
2022-04-25 23:52:26 +0530

    addeda app icon

--- app/src/main/java/com/example/cams/SplashScreen.kt
+++ app/src/main/java/com/example/cams/SplashScreen.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
-class splashscreen : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
